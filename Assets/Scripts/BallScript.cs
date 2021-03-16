a463f8f9b3c747a923273e7910a69768a2d9ea34
YasunoriTomioka <m.h0812y.t@gmail.com>
2021-03-16 12:41:18 +0900

    シーンの情報を削除

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    int speed = 10;　// ここでボールの速さを決めれるよ！数字を上げると早くなって、下げると遅くなる！
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("right") == true)　// 右矢印キーを押した時
+        {
+            // 右方向への移動についてのコード
+            this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+
+
+        //　この下に上下左キーを押したときについてのコードを書いてみよう！
+    }
+}
---
+++ Assets/seikai/BallSeikaiScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSeikaiScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    int speed = 10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("right")){
+            this.transform.position += new Vector3(speed*Time.deltaTime,0,0);
+        }
+
+        if(Input.GetKey("left")){
+            this.transform.position -= new Vector3(speed*Time.deltaTime,0,0);
+        }
+        if(Input.GetKey("up")){
+            this.transform.position += new Vector3(0,0,speed*Time.deltaTime);
+        }
+        if(Input.GetKey("down")){
+            this.transform.position -= new Vector3(0,0,speed*Time.deltaTime);
+        }
+    }
+}
